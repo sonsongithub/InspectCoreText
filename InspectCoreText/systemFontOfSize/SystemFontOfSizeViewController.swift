0f13316939240cee9b89514cdf76574ac1c6cda7
sonson <yoshida.yuichi@gmail.com>
2016-11-09 08:51:59 +0900

    swiftlint.

--- InspectCoreText/systemFontOfSize/SystemFontOfSizeViewController.swift
+++ InspectCoreText/systemFontOfSize/SystemFontOfSizeViewController.swift
@@ -9,35 +9,34 @@
 import UIKit
 
 class SystemFontOfSizeViewController: UIViewController {
-    @IBOutlet var textView:TextView? = nil
-    @IBOutlet var segment:UISegmentedControl? = nil
-    var backview:UIView? = nil
-    
-    @IBAction func didChange(sender:AnyObject) {
+    @IBOutlet var textView: TextView? = nil
+    @IBOutlet var segment: UISegmentedControl? = nil
+    var backview: UIView? = nil
+
+    @IBAction func didChange(sender: AnyObject) {
         if let segment = segment {
             if segment.selectedSegmentIndex == 0 {
                 updateContent(UIFont.systemFontOfSize(12))
-            }
-            else if segment.selectedSegmentIndex == 1 {
+            } else if segment.selectedSegmentIndex == 1 {
                 let font = UIFont.systemFontOfSize(12)
                 updateContent(UIFont(name: font.fontName, size:12)!)
             }
         }
     }
-    
-    func updateContent(renderingFont:UIFont) {
+
+    func updateContent(renderingFont: UIFont) {
         guard let path = NSBundle.mainBundle().pathForResource("data.txt", ofType: nil) else { return }
         guard let data = NSData(contentsOfFile: path) else { return }
         guard let source = NSString(data: data, encoding: NSUTF8StringEncoding) as? String else { return }
         let attr = NSMutableAttributedString(string: source)
-        
+
         attr.addAttribute(NSFontAttributeName, value: renderingFont, range: NSMakeRange(0, attr.length))
-        
+
         if let textView = textView {
             textView.setContent(attr, width: 340)
         }
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         updateContent(UIFont.systemFontOfSize(12))
--- InspectCoreText/systemFontOfSize/TextView.swift
+++ InspectCoreText/systemFontOfSize/TextView.swift
@@ -9,45 +9,45 @@
 import UIKit
 
 class TextView: UIView {
-    var attributedString:NSAttributedString? = nil
-    var framesetter:CTFramesetter? = nil
-    var ctframe:CTFrameRef? = nil
-    var contentRect = CGRectZero
-    
-    func setContent(newAttributedString:NSAttributedString, width:CGFloat) {
-        
-        let newframesetter = CTFramesetterCreateWithAttributedString(newAttributedString);
-        
+    var attributedString: NSAttributedString? = nil
+    var framesetter: CTFramesetter? = nil
+    var ctframe: CTFrameRef? = nil
+    var contentRect = CGRect.zero
+
+    func setContent(newAttributedString: NSAttributedString, width: CGFloat) {
+
+        let newframesetter = CTFramesetterCreateWithAttributedString(newAttributedString)
+
         let size = CTFramesetterSuggestFrameSizeWithConstraints(
             newframesetter,
             CFRangeMake(0, newAttributedString.length),
             nil,
             CGSizeMake(width, CGFloat.max),
             nil)
-        
-        contentRect = CGRectZero
+
+        contentRect = CGRect.zero
         contentRect.size = size
-        
+
         let path = CGPathCreateMutable()
         CGPathAddRect(path, nil, contentRect)
         let newFrame = CTFramesetterCreateFrame(newframesetter, CFRangeMake(0, 0), path, nil)
-        
+
         attributedString = newAttributedString
         framesetter = newframesetter
         ctframe = newFrame
-        
+
         self.setNeedsDisplay()
     }
-    
+
     override func drawRect(rect: CGRect) {
         if let ctframe = ctframe, let context = UIGraphicsGetCurrentContext() {
             // draw text
-            CGContextSaveGState(context);
-            CGContextTranslateCTM(context, 0, contentRect.size.height);
-            CGContextScaleCTM(context, 1.0, -1.0);
-            CGContextSetTextMatrix(context, CGAffineTransformIdentity);
-            CTFrameDraw(ctframe, context);
-            CGContextRestoreGState(context);
+            CGContextSaveGState(context)
+            CGContextTranslateCTM(context, 0, contentRect.size.height)
+            CGContextScaleCTM(context, 1.0, -1.0)
+            CGContextSetTextMatrix(context, CGAffineTransformIdentity)
+            CTFrameDraw(ctframe, context)
+            CGContextRestoreGState(context)
         }
     }
 }
